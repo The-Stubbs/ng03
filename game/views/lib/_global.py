8b04caca775706e6d94c54f8e83ad5903af76930
Freddec <freddec.exileng@gmail.com>
2020-09-13 20:15:27 +0200

    dev

--- game/views/lib/_global.py
+++ game/views/lib/_global.py
@@ -7,8 +7,7 @@ from django.views import View
 
 from game.views.lib.exile import *
 from game.views.lib.template import *
-
-from game.game.cache import *
+from game.views.lib.cache import *
 
 class GlobalView(ExileMixin, View):
 
