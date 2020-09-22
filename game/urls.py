c402440f745ffb36c086c383bca825ccf4d1be68
Freddec <freddec.exileng@gmail.com>
2020-09-22 17:03:16 +0200

    dev

--- game/urls.py
+++ game/urls.py
@@ -4,69 +4,9 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
-'''
-from game.views import error_500
-
-'''
 from game.views import game_connection
-'''
 
-from game.views import alliance
-'''
-from game.views import alliance_new
-'''
-from game.views import alliance_invitations
-from game.views import alliance_manage
-from game.views import alliance_members
-from game.views import alliance_naps
-from game.views import alliance_reports
-from game.views import alliance_tributes
-from game.views import alliance_wallet
-from game.views import alliance_wars
-from game.views import battle
-from game.views import battle_view
-from game.views import buildings
-from game.views import chats
-from game.views import commanders
-from game.views import fleet
-from game.views import fleet_ships
-from game.views import fleet_split
-from game.views import fleet_trade
-from game.views import fleets
-from game.views.lib import fleets_handler
-from game.views import fleets_orbiting
-from game.views import fleets_ships_stats
-from game.views import fleets_standby
-from game.views import game_over
-from game.views import help
-from game.views import holidays
-from game.views import invasion
-from game.views import mails
-from game.views import maintenance
-from game.views import map
-from game.views import market_buy
-from game.views import market_sell
-from game.views import mercenary_intelligence
-from game.views import nation
-from game.views import notes
-from game.views import orbit
-from game.views import options
-from game.views import overview
-from game.views import planets
-from game.views import planet
-from game.views import production
-from game.views import ranking_alliances
-from game.views import ranking_players
-from game.views import reports
-from game.views import research
-from game.views import shipyard
-from game.views import spy_report
-from game.views import start
-from game.views import training
-from game.views import upkeep
-from game.views import wait
-'''
-
+from game.views import empire_view
 
 ################################################################################
 handler500 = 'error_500.View'
@@ -76,6 +16,6 @@ urlpatterns = [
 	path('', RedirectView.as_view(url='/game/connection/')),
     path('connection/', game_connection.View.as_view()),
 	#---------------------------------------------------------------------------
-    path('alliance-new/', alliance_new.View.as_view()),
+    path('empire_view/', empire_view.View.as_view()),
 ]
 ################################################################################
